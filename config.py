dd3564d39f8a694c89c4ce040c8b285946d87f93
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-07-02 23:15:52 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 from pyrogram import Client
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6394084226:AAEd5vCtTrQxhSnOGyPWbHBlsEHtZO2Q1DA")
-API_HASH = os.environ.get("API_HASH", "c23db4aa92da73ff603666812268597a")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_HASH = os.environ.get("API_HASH", "")
 API_ID = os.environ.get("API_ID", 2374174)
 OWNER_ID = os.environ.get("OWNER_ID", "mmagneto")
