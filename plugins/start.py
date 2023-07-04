9a26ff2b56f8fe70dad4df3a3d369853ae82082a
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-07-04 19:01:16 +0300

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -2,5 +2,5 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command("start"))
 async def startm(bot, message):
-    mes = f"Merhaba {message.from_user.mention},\nBu bot sayesinde Wattpad hikayelerini Epub ve Html formatında indirebilirsin iyi kullanımlar..\n\nBot Kullanımı:\nAdım 1: `https://www.wattpad.com/story/221912360-no-26` bu url'den hikayenin kodunu alıyoruz. (221912360)\nAdım 2: `/dl 221912360` komutunu bota yolluyoruz.."
+    mes = f"Merhaba {message.from_user.mention},\nBu bot sayesinde Wattpad hikayelerini Epub ve Html formatında indirebilirsin iyi kullanımlar..\n\nBot Kullanımı:\nAdım 1: https://www.wattpad.com/story/221912360-no-26 bu url'den hikayenin kodunu alıyoruz. (221912360)\n\nAdım 2: `/dl 221912360` komutunu bota yolluyoruz.."
     await message.reply_text(mes)
